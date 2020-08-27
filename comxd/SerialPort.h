2c383d8d2d81b7df481cef26ca65fe024097aa26
chiv <noonecancrackit@163.com>
2020-08-27 18:32:48 +0800

    fix bugs

--- comxd/SerialPort.h
+++ comxd/SerialPort.h
@@ -10,22 +10,38 @@ public:
     //
     size_t Available() const
     {
-        return mSerial->available();
+        size_t sz = 0;
+        try {
+            sz = mSerial->available();
+        } catch (...) {}
+        return sz;
     }
     //
     size_t Read(unsigned char* buf, size_t sz)
     {
-        return mSerial->read(buf, sz);
+        size_t sz_ = 0;
+        try {
+            sz_ = mSerial->read(buf, sz);
+        } catch (...) {}
+        return sz_;
     }
     //
     size_t Write(const unsigned char* buf, size_t sz)
     {
-        return mSerial->write(buf, sz);
+        size_t sz_ = 0;
+        try {
+            sz_ = mSerial->write(buf, sz);
+        } catch (...) {}
+        return sz_;
     }
     //
     std::string DeviceName() const
     {
-        return mSerial->getPort();
+        std::string name;
+        try {
+            name = mSerial->getPort();
+        } catch (...) {}
+        return name;
     }
     //
     serial::Serial* nativeDevice() const { return mSerial.get(); }
