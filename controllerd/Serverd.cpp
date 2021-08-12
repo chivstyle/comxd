b338bfc16d30025437bd6cee0a409edcff91b10e
chivstyle <noonecancrackit@163.com>
2021-08-12 22:16:26 +0800

    add controllerd, this is my private project

---
+++ controllerd/Serverd.cpp
@@ -0,0 +1,74 @@
+//
+// (c) 2021 chiv
+//
+#include "Serverd.h"
+
+using namespace Upp;
+
+WsServerd::Worker::Worker()
+{
+    Ws.NonBlocking();
+}
+
+void WsServerd::Worker::Do(std::function<void(const Upp::String&)> on_message)
+{
+    String text = Ws.Receive();
+    if (!text.IsEmpty()) {
+        on_message(text);
+    }
+}
+
+WsServerd::WsServerd(int port)
+    : mPort(port)
+{
+}
+
+bool WsServerd::SendText(const String& text)
+{
+    bool ok = false;
+    mLock.Enter();
+    for (int i = mWorkers.GetCount() - 1; i >= 0; i--) {
+        ok = ok || mWorkers[i].Ws.SendText(text, true);
+    }
+    mLock.Leave();
+    return ok;
+}
+
+void WsServerd::Run(volatile bool* should_exit)
+{
+    TcpSocket server;
+    // default values: listen_count:5, ipv6:false, reuse:true, just what we need.
+    server.Timeout(0).Listen(mPort);
+    while (*should_exit == false) {
+        SocketWaitEvent sel; // select indeed
+        sel.Add(server, WAIT_READ);
+        // other thread will never add/remove items to/from array, so
+        // we need not lock/unlock here
+        for (auto& w : mWorkers) {
+            w.Ws.AddTo(sel);
+        }
+        sel.Wait(200);
+        mLock.Enter();
+        for (int i = mWorkers.GetCount() - 1; i >= 0; i--) {
+            if (sel[i+1]) {
+                Worker& w = mWorkers[i];
+                w.Do([&](const String& message) {
+                    WhenMessage(w.Ws, message);
+                });
+                if (w.Ws.IsClosed() || w.Ws.IsError()) {
+                    WhenClosed(w.Ws);
+                    mWorkers.Remove(i);
+                }
+            }
+        }
+        mLock.Leave();
+        if (sel[0]) {
+            // add, we should lock and go
+            mLock.Enter();
+            if (!mWorkers.Add().Ws.NonBlocking().Accept(server)) {
+                mWorkers.Drop();
+            }
+            mLock.Leave();
+        }
+    }
+}
---
+++ controllerd/Serverd.h
@@ -0,0 +1,39 @@
+//
+// (c) 2021 chiv
+//
+#ifndef _controllerd_Serverd_h_
+#define _controllerd_Serverd_h_
+
+#include <Core/Core.h>
+#include <functional>
+
+// websocket server
+class WsServerd {
+public:
+    WsServerd(int port);
+    
+    void Run(volatile bool* should_exit);
+    
+    // Send text to all clients
+    // If any send was succeeded, return true
+    bool SendText(const Upp::String&);
+    //
+    Upp::Event<Upp::WebSocket&, const Upp::String&> WhenMessage;
+    //
+    Upp::Event<Upp::WebSocket&> WhenClosed;
+    
+private:
+    int mPort;
+    //-----------------------------------------------------------
+    class Worker {
+    public:
+        Worker();
+        Upp::WebSocket Ws;
+        void Do(std::function<void(const Upp::String&)> on_message);
+    };
+    Upp::Array<Worker> mWorkers;
+    Upp::Mutex mLock;
+    //-----------------------------------------------------------
+};
+
+#endif
