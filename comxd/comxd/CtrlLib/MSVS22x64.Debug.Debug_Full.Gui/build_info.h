1ba1078438255cc91e16a472e62e53276921acd3
chivstyle <noonecancrackit@163.com>
2022-01-30 17:38:48 +0800

    pipe

--- comxd/comxd/CtrlLib/MSVS22x64.Debug.Debug_Full.Gui/build_info.h
+++
@@ -1,9 +0,0 @@
-#define bmYEAR   2022
-#define bmMONTH  1
-#define bmDAY    29
-#define bmHOUR   11
-#define bmMINUTE 27
-#define bmSECOND 44
-#define bmTIME   Time(2022, 1, 29, 11, 27, 44)
-#define bmMACHINE "DESKTOP-9FE9LFL"
-#define bmUSER    "chiv"
--- terminal/comxd/Painter/CLANGx64.Debug.Debug_Full.Gui/build_info.h
+++
@@ -1,9 +0,0 @@
-#define bmYEAR   2022
-#define bmMONTH  1
-#define bmDAY    29
-#define bmHOUR   11
-#define bmMINUTE 31
-#define bmSECOND 37
-#define bmTIME   Time(2022, 1, 29, 11, 31, 37)
-#define bmMACHINE "DESKTOP-9FE9LFL"
-#define bmUSER    "chiv"
